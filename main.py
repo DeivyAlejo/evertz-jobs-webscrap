4b3fb4f48ffe9e1010a768cb9b84d8ac3665924e
Deivy Munoz <dmunoz2460@gmail.com>
2025-09-11 17:47:48 -0400

    add new options to scrap webpage

--- main.py
+++ main.py
@@ -1,113 +1,122 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import time
 
-url = "https://evertz.com/contact/careers/"
+# Check out this other link: http://evertz.applytojob.com/apply/
 
+url = "http://evertz.applytojob.com/apply/"
 response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+jobs = soup.find('ul', class_='list-group')
+li_jobs = jobs.find_all(class_='list-group-item')
 
-html = '''
-<ul class="no-marker ps-0" id="job_results">
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250707192319_IESJHVVVDJL5XLCW" id="job_20250707192319_IESJHVVVDJL5XLCW">Administrative Assistant (Entry Level)</a></h6>
-<p>Administration<br/>Burlington, ON, Canada</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250708134504_JLMCGYSBMHJJNGOM" id="job_20250708134504_JLMCGYSBMHJJNGOM">Broadcast Engineer - Pittsburgh</a></h6>
-<p><br/>Indiana/Pittsburgh, PA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250708152218_ISSNNDNKR2ULGOIF" id="job_20250708152218_ISSNNDNKR2ULGOIF">Broadcast Engineer, Burbank, California</a></h6>
-<p>Technical Support<br/>Burbank, CA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250708135025_VWACJLG6LNGLUANH" id="job_20250708135025_VWACJLG6LNGLUANH">Broadcast Engineer, Long Island, New York</a></h6>
-<p>Technical Support<br/>Hauppauge, Long Island, NY, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250708135613_ML1MT6WWEVAZ8P2T" id="job_20250708135613_ML1MT6WWEVAZ8P2T">Broadcast Engineer, New York</a></h6>
-<p>USA<br/>New York, NY, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250707174812_RJPZM5VMZCR3Q9QB" id="job_20250707174812_RJPZM5VMZCR3Q9QB">Customer Order Coordinator</a></h6>
-<p>Administration<br/>Burlington, ON, Canada</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250414173001_RCN7CXAL6JESOPCM" id="job_20250414173001_RCN7CXAL6JESOPCM">DSP Engineer</a></h6>
-<p><br/>Pittsburgh, PA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250630123406_AQCRWBX3Y2DC5JGJ" id="job_20250630123406_AQCRWBX3Y2DC5JGJ">Electronics Manufacturing Inspector - Evertz USA</a></h6>
-<p>SMT<br/>Indiana, PA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250630123156_AIE6ONWLKZEDNYCR" id="job_20250630123156_AIE6ONWLKZEDNYCR">Electronics Manufacturing Operator, Evertz USA</a></h6>
-<p>SMT<br/>Indiana, PA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250630123231_VFNCKKYPRT6S0SX2" id="job_20250630123231_VFNCKKYPRT6S0SX2">Electronics Manufacturing Programmer, Evertz USA</a></h6>
-<p>SMT<br/>Indiana, PA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250331131622_BYLKABUIWLDIYNNQ" id="job_20250331131622_BYLKABUIWLDIYNNQ">Embedded Software Engineer - Pittsburgh</a></h6>
-<p>R&amp;D<br/>Pittsburgh, PA, United States</p>
-</li>
-<li class="mb-3 pb-3 border-bottom border-1 border-secondary no-replace">
-<h6><a data-bs-target="#modalJobDetails" data-bs-toggle="modal" href="/contact/careers/job_20250127165234_MHKMCENYRVQELPNR" id="job_20250127165234_MHKMCENYRVQELPNR">Enterprise Support Engineer</a></h6>
-<p><br/>Warsaw, Poland</p>
-</li>
-</ul>
-'''
+job_list = []
 
-# soup = BeautifulSoup(html, "html.parser")
+for li in li_jobs:
+    h4 = li.find('h4')
+    a_tag = h4.find('a')
+    job_title = a_tag.get_text(strip=True)
+    job_link = a_tag['href']
+    inner_ul = li.find('ul')
+    inner_li_items = inner_ul.find_all('li')
+    details = [item.get_text(strip=True) for item in inner_li_items]
 
-# # print(soup)
+    if len(details) <2:
+        details.append("")
+
+    job_list.append({
+            'Job name':job_title,
+            'Location': details[0],
+            'Department':details[1],
+            'Link': job_link
+        })
+
+with open('jobs_new.json', 'w', encoding='utf-8') as f:
+    json.dump(job_list, f, ensure_ascii=False, indent=4)
+
+print("Job data saved to jobs.json")
+
+# print(jobs)
+
+# This code is for this url "https://evertz.com/contact/careers/"
+# url = "https://evertz.com/contact/careers/"
+
+# response = requests.get(url)
+
+# soup = BeautifulSoup(response.text, "html.parser")
 
 # pagination = soup.find(id="job_paginate")
 
-# # print(pagination)
-
 # multiple_pages = pagination.get("class")
 
-soup = BeautifulSoup(html, "html.parser")
-jobs = soup.find(id="job_results")
-# print(jobs)
-li_jobs = jobs.find_all("li")
+# jobs = soup.find(id="job_results")
 
-# li_job = li_jobs[12]
-# a_tag = li_job.find("a")
-# job_name = a_tag.get_text(strip=True)
-# href_link = a_tag['href']
-# p_tag = li_job.find('p')
-# p_tag = str(p_tag)[3:-4]
-# details = p_tag.split("<br/>")
-# print(details)
-for li_job in li_jobs:
-    a_tag = li_job.find("a")
-    # print(a_tag)
-    job_name = a_tag.get_text(strip=True)
-    href_link = a_tag['href']
-    p_tag = li_job.find('p')
-    p_tag = str(p_tag)[3:-4]
-    details = p_tag.split("<br/>")
-
-    print(f"Job: {job_name}")
-    print(f"Department: {details[0]}")
-    print(f"Location: {details[1]}")
-    print("-----------------------")
+# li_jobs = jobs.find_all("li")
 
 
-
-
-
-
-# This is to extract all the listing when I know how to extract one. Do not delete this
+# # This is to extract all the listing when I know how to extract one. Do not delete this
+# list_of_urls = []
+# base_url = 'https://evertz.com/contact/careers/?page='
 # if (not multiple_pages):
 #     # print("Multiple pages")
 
 #     li_items = pagination.find_all("li")
 
 #     for li in li_items:
-#         print(li.get_text(strip=True))
+#         try:
+#             page = int(li.get_text(strip=True))
+#             list_of_urls.append(base_url+(str(page-1)))
+#         except ValueError:
+#             # Code that runs if an exception occurs
+#             print("Something went wrong!")
+
+#         # print(type(li.get_text))
+
+# job_list = []
+
+# for url in list_of_urls:
+#     time.sleep(1)
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     jobs = soup.find(id="job_results")
+#     li_jobs = jobs.find_all("li")
+
+#     print(url)
+
+#     for li_job in li_jobs:
+#         a_tag = li_job.find("a")
+#         # print(a_tag)
+#         a_id = a_tag.get('id')
+#         job_name = a_tag.get_text(strip=True)
+#         href_link = a_tag['href']
+#         p_tag = li_job.find('p')
+#         p_tag = str(p_tag)[3:-4]
+#         details = p_tag.split("<br/>")
+
+#         # print(f"ID: {a_id}")
+#         # print(f"Job: {job_name}")
+#         # print(type(details[0]))
+#         # print(f"Department: {details[0]}")
+#         # print(type(details[1]))
+#         # print(f"Location: {details[1]}")
+#         # print(f"Link: www.evertz.com{href_link}")
+#         # print("-----------------------")
+        
+
+#         job_list.append({
+#             'id':a_id,
+#             'Job name':job_name,
+#             'Department':details[0],
+#             'Location': details[1],
+#             'Link': 'www.evertz.com'+ href_link
+#         })
+
+# with open('jobs.json', 'w', encoding='utf-8') as f:
+#     json.dump(job_list, f, ensure_ascii=False, indent=4)
+
+# print("Job data saved to jobs.json")
+
 
 # print(pagination.get("class"))
 
