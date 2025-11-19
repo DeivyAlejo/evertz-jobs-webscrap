9864791ed1ff16a82f01ff78d032069ea3818e47
Deivy Munoz <dmunoz2460@gmail.com>
2025-11-19 13:06:23 -0500

    add create_initial_json python script

---
+++ create_initial_json.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+from datetime import datetime
+import os
+
+from main import scrape_jobs, save_jobs
+
+if __name__ == "__main__":
+    try:
+        jobs = scrape_jobs(),
+        save_jobs(jobs)
+    except Exception as e:
+        print(e)
